958301010850d4c1e078a4ad9d6c93a526ca9fc5
Svetlana Govyazina <govyazina@gmail.com>
2022-12-09 15:25:28 +0200

    files for day 5 added

---
+++ day5.test.js
@@ -0,0 +1,14 @@
+const stacks = require('./day5');
+
+const input = `
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+test('test', () => {expect(stacks(input)).toBe('CMZ')});
