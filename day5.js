09d9331bad077efe2d73ecfb9c8d3c8d76376f7f
Svetlana Govyazina <govyazina@gmail.com>
2022-12-09 16:09:26 +0200

    day 5 changed input

--- day5.js
+++ day5.js
@@ -1,3 +1,3 @@
-module.exports = (input) => {
+module.exports = (startStacks, input) => {
 
 }
--- day5.test.js
+++ day5.test.js
@@ -1,14 +1,14 @@
 const stacks = require('./day5');
 
-const input = `
+const startStacks = `
     [D]    
 [N] [C]    
 [Z] [M] [P]
- 1   2   3 
-
+ 1   2   3 `;
+const input = `
 move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2`;
 
-test('test', () => {expect(stacks(input)).toBe('CMZ')});
+test('test', () => {expect(stacks(startStacks, input)).toBe('CMZ')});
