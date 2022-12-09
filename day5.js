d4779b810d663d921dbc4ab250a0996a43c2f117
Svetlana Govyazina <govyazina@gmail.com>
2022-12-09 16:51:17 +0200

    day 5 function added (light input)

--- day5.js
+++ day5.js
@@ -1,3 +1,15 @@
-module.exports = (startStacks, input) => {
-
+module.exports = (stacks, input) => {
+    const procedures = input.split('\n').map(e => e.split(' '));
+    for (const procedure of procedures) {
+        const boxes = Number(procedure[1]);
+        for (let i = 0; i < boxes; i++) {
+            let box = stacks[procedure[3]].pop();
+            stacks[procedure[5]].push(box);
+        }
+    }
+    let string = '';
+    for (const stack of stacks) {
+        string += stack.pop();
+    }
+    return string.trim();
 }
--- day5.test.js
+++ day5.test.js
@@ -1,14 +1,21 @@
 const stacks = require('./day5');
 
-const startStacks = `
-    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 `;
+const startStacks =
+    [
+        [' '],
+        ['Z', 'N'],
+    ['M', 'C', 'D'],
+    ['P']
+    ];
+// const startStacks = `
+//     [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3 `;
 const input = `
 move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
-move 1 from 1 to 2`;
+move 1 from 1 to 2`.trim();
 
 test('test', () => {expect(stacks(startStacks, input)).toBe('CMZ')});
