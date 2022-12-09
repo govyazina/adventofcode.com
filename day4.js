7948bcb1e89e14b260f049534c7aa02c527a991e
Svetlana Govyazina <govyazina@gmail.com>
2022-12-09 14:06:45 +0200

    function for day 4 added

--- day4.js
+++ day4.js
@@ -1,3 +1,12 @@
 module.exports = (input) => {
-
+    const pairs = input.split('\n').map(e => e.split(',').map(e => e.split('-').map(e => Number(e))));
+    let sum = 0;
+    for (const pair of pairs) {
+        let cond = pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]
+            || pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1];
+        if (cond) {
+            sum++
+        }
+    }
+    return sum;
 }
