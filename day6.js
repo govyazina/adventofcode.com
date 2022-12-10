b20eefe98d71f41a2f398214734de677d9bddb9c
Svetlana Govyazina <govyazina@gmail.com>
2022-12-10 17:48:56 +0200

    day 6: function changed

--- day6.js
+++ day6.js
@@ -2,14 +2,24 @@ module.exports = (input) => {
     let marker;
     for (let i = 0; i < input.length; i++) {
         const findMarker = input.slice(i, i + 4);
-        const a = findMarker[0];
-        const b = findMarker[1];
-        const c = findMarker[2];
-        const d = findMarker[3];
-        if (a !== b && a !== c && a !== d && b !== c && b !== d && c !== d) {
-            marker = i+4;
-            break;
+        if (isMarker(findMarker)) {
+            marker = i + 4;
+            return marker
         }
     }
     return marker;
+}
+
+function isMarker (marker) {
+    const obj = {}
+    for (let i = 0; i < marker.length; i++) {
+        let letter = marker[i];
+        if (obj[letter]) {
+            return false;
+        }
+        else {
+            obj[letter] = true;
+        }
+    }
+    return true;
 }
