401058a783ecd46ca95d70fa27a46ed24aacb2f7
Svetlana Govyazina <govyazina@gmail.com>
2022-12-10 17:09:27 +0200

    day 6: function added

--- day6.js
+++ day6.js
@@ -1,3 +1,15 @@
 module.exports = (input) => {
-
+    let marker;
+    for (let i = 0; i < input.length; i++) {
+        const findMarker = input.slice(i, i + 4);
+        const a = findMarker[0];
+        const b = findMarker[1];
+        const c = findMarker[2];
+        const d = findMarker[3];
+        if (a !== b && a !== c && a !== d && b !== c && b !== d && c !== d) {
+            marker = i+4;
+            break;
+        }
+    }
+    return marker;
 }
--- day6.test.js
+++ day6.test.js
@@ -2,8 +2,8 @@ const radioSignal = require('./day6');
 
 // const input = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`;
 
-test('test-1', () => {expect(radioSignal(startStacks, `mjqjpqmgbljsphdztnvjfqwrcgsmlb`)).toBe('7')});
-test('test-2', () => {expect(radioSignal(startStacks, 'bvwbjplbgvbhsrlpgdmjqwftvncz')).toBe('5')});
-test('test-3', () => {expect(radioSignal(startStacks, 'nppdvjthqldpwncqszvftbrmjlhg')).toBe('6')});
-test('test-4', () => {expect(radioSignal(startStacks, 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')).toBe('10')});
-test('test-5', () => {expect(radioSignal(startStacks, 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw')).toBe('11')});
+test('test-1', () => {expect(radioSignal(`mjqjpqmgbljsphdztnvjfqwrcgsmlb`)).toBe(7)});
+test('test-2', () => {expect(radioSignal('bvwbjplbgvbhsrlpgdmjqwftvncz')).toBe(5)});
+test('test-3', () => {expect(radioSignal('nppdvjthqldpwncqszvftbrmjlhg')).toBe(6)});
+test('test-4', () => {expect(radioSignal('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')).toBe(10)});
+test('test-5', () => {expect(radioSignal('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw')).toBe(11)});
